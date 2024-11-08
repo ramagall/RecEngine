d23ab3ced3e392f6d00a039f7f032ecf75df3a76
ramagall <rmagallanesbarreras@gmail.com>
2024-11-08 08:40:02 -0700

    Responsive
    Everything on the landing page seems to be responsive now

--- lamb-telecom-rec-engine/src/LandingPageComponents/LandingPageTextFields.jsx
+++ lamb-telecom-rec-engine/src/LandingPageComponents/LandingPageTextFields.jsx
@@ -6,83 +6,86 @@ function LandingPageTextFields() {
     return(
         <>
         <div className="textFields">
-            <Box className="textFields"
-            component="form"
-            sx={{ '& .MuiTextField-root': { m: 1, width: '50ch'} }}
-            noValidate
-            autoComplete="off"
-            >
-                <div class='textField'>
+                <div className='textFieldHelper'>
                 <TextField 
                 size ='small' 
                 id="first-name-field" 
                 label="First Name" 
                 variant="outlined" 
+                className="textField"
                 />
                 <TextField 
                     size ='small' 
                     id="last-name-field" 
                     label="Last Name" 
                     variant="outlined" 
+                    className="textField"
                     />
                 </div>
-                <div>
+                <div className='textFieldHelper'>
                 <TextField 
                     size ='small' 
                     id="phone-number-field" 
                     label="Phone Number" 
                     variant="outlined" 
+                    className="textField"
                     />
                 <TextField 
                     size ='small' 
                     id="email-field" 
                     label="Email" 
-                    variant="outlined" 
+                    variant="outlined"
+                    className="textField"
                     />
                 </div>
-                <div>
+                <div className='textFieldHelper'>
                 <TextField
                     size ='small' 
                     id="org-name-field" 
                     label="Organization Name" 
                     variant="outlined" 
+                    className="textField"
                     />
                 <TextField 
                     size ='small' 
                     id="address-field-one" 
                     label="Address Line 1" 
                     variant="outlined" 
+                    className="textField"
                     />
                 </div>
-                <div>
+                <div className='textFieldHelper'>
                 <TextField
                     size ='small' 
                     id="address-field-2" 
                     label="Address Line 2" 
                     variant="outlined" 
+                    className="textField"
                     />
                 <TextField 
                     size ='small' 
                     id="city-field" 
                     label="City" 
                     variant="outlined" 
+                    className="textField"
                     />
                 </div>
-                <div>
+                <div className='textFieldHelper'>
                 <TextField
                     size ='small' 
                     id="state-field" 
                     label="State" 
                     variant="outlined" 
+                    className="textField"
                     />
                 <TextField 
                     size ='small' 
                     id="zip-code-field" 
                     label="Zip Code" 
                     variant="outlined" 
+                    className="textField"
                     />
                     </div>
-            </Box>
             </div>
         </>
     );
