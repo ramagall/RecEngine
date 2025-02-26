043ff7f916057914dbcd8507c50e9ad20719648a
zachhstewart <zachhstewart@gmail.com>
2025-02-26 11:53:33 -0700

    double checkbox selection fixed

--- rec-engine/frontend/src/internet/internet1.jsx
+++ rec-engine/frontend/src/internet/internet1.jsx
@@ -7,10 +7,15 @@ const Internet = () => {
   const [selectedOptions, setSelectedOptions] = useState([]);
   const navigate = useNavigate();
 
+  const [internetCompaniesConsidered, setInternetCompaniesConsidered] = useState([]);
+  const [internetProviderUsiing, setInternetProviderUsing] = useState([]);
+  const [internetRouters, setInternetRouters] = useState([]);
+
+
   // Handle Checkbox Selection
-  const handleCheckboxChange = (event) => {
+  const handleCheckboxChange = (setOptions) => (event) => {
     const { value, checked } = event.target;
-    setSelectedOptions((prev) =>
+    setOptions((prev) =>
       checked ? [...prev, value] : prev.filter((item) => item !== value)
     );
   };
@@ -31,22 +36,22 @@ const Internet = () => {
       <CheckboxGroup
         label="What internet companies have you already considered?"
         options={["AT&T", "Spectrum", "Comcast", "Zayo", "Earthlink", "Crown Castle", "Cox", "Lumen", "Geolinks", "Other:"]}
-        selectedOptions={selectedOptions}
-        onChange={handleCheckboxChange}
+        selectedOptions={internetCompaniesConsidered}
+        onChange={handleCheckboxChange(setInternetCompaniesConsidered)}
         columns={2}
       />
       <CheckboxGroup
         label="What internet provider are you already using?"
         options={["AT&T", "Spectrum", "Comcast", "Zayo", "Earthlink", "Crown Castle", "Cox", "Lumen", "Geolinks", "Other:"]}
-        selectedOptions={selectedOptions}
-        onChange={handleCheckboxChange}
+        selectedOptions={internetProviderUsiing}
+        onChange={handleCheckboxChange(setInternetProviderUsing)}
         columns={2}
       />
       <CheckboxGroup
         label="Will you need routers or managed circuits/switches?"
         options={["Yes", "No", "Unsure"]}
-        selectedOptions={selectedOptions}
-        onChange={handleCheckboxChange}
+        selectedOptions={internetRouters}
+        onChange={handleCheckboxChange(setInternetRouters)}
       />
       {/* Button with proper navigation */}
       <Button
